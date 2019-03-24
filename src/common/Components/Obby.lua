ed2c724ee5ee9feaf002e5d15d26c03d529f7831
Nim <areuschle1@gmail.com>
2019-03-24 01:37:33 -0500

    added more coins to stages 3-5

--- src/common/Components/Obby.lua
+++ src/common/Components/Obby.lua
@@ -7,7 +7,7 @@ local RECS = require(lib:WaitForChild("RECS"))
 
 return RECS.defineComponent("Obby", function()
     return {
-        startingStageIndex = 6,
+        startingStageIndex = 1,
         activeStageIndex = nil,
         activeStageInstance = nil,
         activeStageComponent = nil,
